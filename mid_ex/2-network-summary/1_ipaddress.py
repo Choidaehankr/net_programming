b896abe37529c786e87100cb440fc7a0bfc07621
Choidaehan <71701634+Choidaehankr@users.noreply.github.com>
2022-04-15 04:39:17 +0900

    [review] network programming module

--- mid_ex/2-network-summary/1_ipaddress.py
+++ mid_ex/2-network-summary/1_ipaddress.py
@@ -9,11 +9,11 @@
 
 import ipaddress
 net = ipaddress.ip_network('114.71.220.0/24')
-print(net)
-print(net.with_netmask)
-print(net.num_addresses)
+# print(net)
+# print(net.with_netmask)
+# print(net.num_addresses)
 
-# 네트워크에서 사용 가능한 호스트 주소 알아보기
+# # 네트워크에서 사용 가능한 호스트 주소 알아보기
 for x in net.hosts():
     print(x)
 
--- mid_ex/2-network-summary/2_socket.py
+++ mid_ex/2-network-summary/2_socket.py
@@ -1,16 +1,19 @@
-import socket
+# import socket
 
-socket.gethostname()  # 실행 중인 호스트 이름을 반환
+# socket.gethostname()  # 실행 중인 호스트 이름을 반환
 # socket.gethostbyname(hostnam) # hostname을 IPv4 주소로 반환
 
 
 # 18p. 여러 사이트의 IP 주소를 확인
 
+import socket
+
 # HOSTS = [
 #     'www.google.com',
-#     'www.sch.ac.kr',
-#     'www.naver.com',
-#     'www.daum.net'
+#     'choikorea.tistory.com',
+#     'www.inflearn.com',
+#     'www.acmicpc.net',
+#     'www.daehanchoigo.com'  # 없는 주소
 # ]
 
 # for host in HOSTS:
@@ -25,17 +28,29 @@ socket.gethostname()  # 실행 중인 호스트 이름을 반환
 # print(socket.getservbyport(25))
 
 # for port in [80, 443, 21, 25, 143, 993, 110, 995]:
-#     url = '{}://excample.co.kr/'.format(socket.getservbyport(port))
+#     url = '{}://choikorea.tistory.com/'.format(socket.getservbyport(port))
 #     print('{:4d}'.format(port), url)
 
     # 24p. 바이트  순서 변환 함수
 
-a = 1234
-print(hex(a))
+# a = 1234
+# print(hex(a))
 
-b = socket.htons(a)  # 2바이트 양의 정수를 호스트 바이트 순서에서 네트워크 바이트 순서로 변환
+# b = socket.htons(a)  # 2바이트 양의 정수를 호스트 바이트 순서에서 네트워크 바이트 순서로 변환
 
-print(hex(b))
+# print(hex(b))
 
-c = socket.ntohs(b)  # 2바이트 양의 정수를 네트워크 바이트 순서에서 호스트 바이트 순서로 변환
-print(hex(c))
+# c = socket.ntohs(b)  # 2바이트 양의 정수를 네트워크 바이트 순서에서 호스트 바이트 순서로 변환
+# print(hex(c))
+
+
+# ipSample = '192.168.0.1'
+# ipToBytes = socket.inet_aton(ipSample)
+# print(ipToBytes)
+
+import struct
+
+ipLong = 168420330
+ipBytes = struct.pack('!I', ipLong)
+ipQuad = socket.inet_ntoa(ipBytes)
+print(ipQuad)
