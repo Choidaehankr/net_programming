c44404d4222669c9319e105fe88958a9a9a7b320
Daehan Choi <mnnm22@naver.com>
2022-06-14 08:55:36 +0900

    study

---
+++ zzz/client.py
@@ -0,0 +1,27 @@
+import asyncio
+from socket import *
+
+port = 2500
+BUFSIZE = 1024
+
+async def send_task(sock):
+    while True:
+        msg = await asyncio.to_thread(input, '메세지를 입력하세요:')
+        sock.send(msg.encode())
+
+async def recv_task(sock):
+    while True:
+        data = await asyncio.to_thread(sock.recv, BUFSIZE)
+        if not data:
+            break
+        print(f'받은 메세지: ', data.decode())
+
+async def main():
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect(('localhost', port))
+    await asyncio.gather(
+            send_task(sock),
+            recv_task(sock)
+    )
+
+asyncio.run(main())
